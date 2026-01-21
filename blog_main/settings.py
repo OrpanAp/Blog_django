af2903eadcbff5d639e84e9aa971df20ea9b3c17
OrpanAp <purificationalex90@gmail.com>
2026-01-21 09:30:02 -0500

    blog app & blog  model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
---
+++ blogs/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Category, Blogs
+
+# Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields= {'slug': ('title',)}
+    list_display = ('title', 'category', 'author', 'status', 'is_featured')
+    list_editable = ('status', 'is_featured')
+    search_fields = ('title', 'category__category_name', 'author__username', 'status')
+    
+
+admin.site.register(Category)
+admin.site.register(Blogs, BlogAdmin)
